b217c3d65d89e8f94a8fbbc91c2ba67f49ccccec
Quienten <44511810+Quienten@users.noreply.github.com>
2024-01-16 11:24:56 -0800

    Shuffle workshop

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
+import {presetGroup} from "./data/preset1.json"
+import { students } from "./data/students.json"
+
+function Shuffled({ shuffledGroups }) {
+    return (
+        <ul className="flex flex-wrap gap-8">
+           {shuffledGroups.map((group, index) => {
+               return <ul className="grid grid-rows-5 grid-flow-col gap-4">
+                   <li className="font-bold text-xl" key={index}>{"Group " + (index+1)}</li>
+                   {group.members.map((student, index2) => {
+                       return <li key={index2} className="w-72 text-xl outline outline-1 outline-cyan-600">{student}</li>
+                   })}
+               </ul>
+           })}
+        </ul>
+    )
+}
+
+function StudentList({ students, onRandomize }) {
+
+    return (
+        <>
+            <ul className="grid grid-cols-4 gap-4">
+                {students.map((student, i) => {
+                    return <li
+                        className="w-72 text-xl outline outline-1 outline-cyan-600"
+                        key={i}
+                    >
+                        {student}
+                    </li>
+                })}
+            </ul>
+            <button className="mt-24 text-3xl font-bold outline outline-1 outline-white" onClick={onRandomize} onContextMenu={onRandomize}>Randomize</button>
+        </>
+    )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [randomized, setRandomized] = useState(false)
+    const [groups, setGroups] = useState(presetGroup)
 
-  return (
+    function generateRandomGroup() {
+        let studentsLeft = students
+        let groups = []
+        let group = []
+        while(studentsLeft.length > 0) {
+            const index = Math.floor(Math.random()*studentsLeft.length)
+            group.push(studentsLeft[index])
+            studentsLeft.splice(index, 1)
+            if(group.length === 4) {
+                groups.push({"members": group})
+                group = []
+            }
+        }
+        if(group.length > 0) {
+            groups.push({"members": group})
+        }
+        return groups;
+    }
+
+    const onRandomize = (e) => {
+        if(e.type === "click") {
+            console.log("Hi");
+            // setGroups([{
+            //     "members": [
+            //         "Hi"
+            //     ]
+            // }])
+            setGroups(generateRandomGroup())
+        } else if(e.type === "contextmenu") {
+            setGroups(presetGroup)
+        }
+        setRandomized(true)
+
+    }
+
+    return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+          {randomized ? <Shuffled shuffledGroups={groups}/> : <StudentList students={students} onRandomize={onRandomize}/>}
+      </main>
     </>
-  )
+    )
 }
 
 export default App
